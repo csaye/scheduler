25b4ab2714a4c84c97da5a8fbb53b3ac5821d812
Cooper Saye <coopersaye@gmail.com>
2020-10-22 23:08:08 -0700

    Implement file writing.

--- scheduler.py
+++ scheduler.py
@@ -5,6 +5,12 @@ running = True
 while (running):
     userInput = input("> ")
     if (userInput == "help"):
-        print("Commands: help, quit")
+        print("Commands: help, quit, new")
     elif (userInput == "quit"):
         running = False;
+    elif (userInput == "new"):
+        f = open("schedule.txt", "a")
+        f.write("hello\n")
+        f.close()
+    else:
+        print("Invalid command. Type \"help\" for help.")
