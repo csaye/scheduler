d1b64e4c5e7d8ea06ba6da87c48193aa84c57722
Cooper Saye <coopersaye@gmail.com>
2020-10-22 23:28:51 -0700

    Implement title and date parsing.

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,53 @@
+import datetime
+
 print("Welcome to scheduler! Type \"help\" for help.")
 
 running = True
+
+def getTitle():
+    title = ":"
+    while (title.find(":") != -1):
+        title = input("Please enter an event title: ")
+    return title
+
+def getDate():
+    valid = False
+    while (valid == False):
+        try:
+            year = int(input("Please enter a year: "))
+            valid = True
+        except:
+            continue
+    valid = False
+    while (valid == False):
+        try:
+            month = int(input("Please enter a month: "))
+            valid = True
+        except:
+            continue
+    valid = False
+    while (valid == False):
+        try:
+            day = int(input("Please enter a day: "))
+            valid = True
+        except:
+            continue
+    try:
+        return datetime.datetime(year, month, day)
+    except:
+        return getDate()
+
+def write():
+    f = open("schedule.txt", "a")
+    title = getTitle()
+    date = getDate().strftime("%x")
+    f.write(title + ":" + date + "\n")
+    f.close()
+
+def read():
+    f = open("schedule.txt", "r")
+    print(f.read())
+    f.close()
 
 while (running):
     userInput = input("> ")
@@ -9,12 +56,8 @@ while (running):
     elif (userInput == "quit"):
         running = False;
     elif (userInput == "write"):
-        f = open("schedule.txt", "a")
-        f.write("hello\n")
-        f.close()
+        write()
     elif (userInput == "read"):
-        f = open("schedule.txt", "r")
-        print(f.read())
-        f.close()
+        read()
     else:
         print("Invalid command. Type \"help\" for help.")
