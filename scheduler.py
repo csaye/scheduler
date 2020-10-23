1b3314f3da8ce3e2f8af5b480052b5c07c3d6152
Cooper Saye <coopersaye@gmail.com>
2020-10-22 23:10:11 -0700

    Implement file reading.

--- scheduler.py
+++ scheduler.py
@@ -5,12 +5,16 @@ running = True
 while (running):
     userInput = input("> ")
     if (userInput == "help"):
-        print("Commands: help, quit, new")
+        print("Commands: help, quit, write, read")
     elif (userInput == "quit"):
         running = False;
-    elif (userInput == "new"):
+    elif (userInput == "write"):
         f = open("schedule.txt", "a")
         f.write("hello\n")
+        f.close()
+    elif (userInput == "read"):
+        f = open("schedule.txt", "r")
+        print(f.read())
         f.close()
     else:
         print("Invalid command. Type \"help\" for help.")
